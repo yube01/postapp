40ab9bd08cbc9781973d26e61afcf238724c932f
yube01 <114811769+yube01@users.noreply.github.com>
2023-05-09 19:47:40 +0545

    register page

--- client/src/page/Register.jsx
+++ client/src/page/Register.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input required type="text" placeholder="username" name="username" />
+        <input required type="email" placeholder="email" name="email" />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+        />
+        <button>Register</button>
+
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
